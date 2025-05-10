d4c259630e2979a60e0348f2c6f246ae6295f0d1
Liry <learainccc@outlook.com>
2025-05-10 21:42:47 +0800

    去掉scss扩展

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,12 @@ import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { config, configs } from 'typescript-eslint';
 
-export default tseslint.config(
+export default config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
